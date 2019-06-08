c3ebd9fef9d43a0be8b3017e320d2d815ff28e82
Emmanuel Eze <emereuwaonueze@gmail.com>
2019-06-08 06:44:37 +0100

    Updates

---
+++ precache-manifest.aa9364adfdcd5b01d298f94436aed904.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ebda5dde4eb908b2ea36",
+    "url": "/static/js/main.59efb921.chunk.js"
+  },
+  {
+    "revision": "ef6eed4447e3b521d1c1",
+    "url": "/static/js/2.da5392a4.chunk.js"
+  },
+  {
+    "revision": "ebda5dde4eb908b2ea36",
+    "url": "/static/css/main.0443c565.chunk.css"
+  },
+  {
+    "revision": "ef6eed4447e3b521d1c1",
+    "url": "/static/css/2.538d099f.chunk.css"
+  },
+  {
+    "revision": "1a347cbcdf8815b7cea2287438f9ab83",
+    "url": "/index.html"
+  }
+];
